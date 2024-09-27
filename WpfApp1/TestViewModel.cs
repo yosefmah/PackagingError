af6102e51f73c090f3c7d0d63097a0195e786d78
Yosef Mahmood <YMahmood@opus2.com>
2024-09-27 18:47:44 +0100

    Add project files.

---
+++ WpfApp1/TestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1;
+public partial class TestViewModel : INotifyPropertyChanged
+{
+    public string SampleProp { get; set; } = "Sample";
+
+}
